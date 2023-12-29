a8733cadf21a0fdc96d5192fb9910135d88d0133
杨旭垲 <11280309+yang-xukai@user.noreply.gitee.com>
2023-12-29 22:03:27 +0800

    创建超参数函数

--- train.py
+++ train.py
@@ -2,4 +2,13 @@ import argparse
 
 def createParse():
     parser = argparse.ArgumentParser(description="training Hyperparameters")
-    parser.add_argument('--alpha','')
+    parser.add_argument('--alpha', default=1e-5, type=float, help='learning rate')
+    parser.add_argument('--weights', default='non_pretrained', type=str, help='non_pretrained, pretrained')
+    parser.add_argument('--device', default='CPU', type=str, help='CPU, 0, 1, 2')
+    parser.add_argument('--batch_size', default=20, type=int, help='the batch size')
+    parser.add_argument('--num_workers', default=0, type=int, help='number of workers')
+    parser.add_argument('--epoch', default=5, type=int, help='number of training loops')
+
+    args = parser.parse_args()
+    return args
+
